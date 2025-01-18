40e62b2d6e10efaac46e951c75caae543b1b6ad7
zaneahmed1 <zaneahmed4@gmail.com>
2025-01-17 20:54:49 -0600

    add input and basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [word, setWord] = useState("");
+
   return (
-    <div className="App">
-      test 1 2 3
+    <div className="App" >
+      <header>
+        <h1>Synomize</h1>
+      </header>
+      <main>
+        <div className="synonym-fetcher">
+          <label htmlFor="word-input">Enter Word:</label>
+          <input
+            id="word-input"
+            type="text"
+            placeholder="e.g., happy"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            aria-label="Input word to find synonyms"
+          />
+          <button disabled={!word.trim()}>
+            {"Get Synonym"}
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
